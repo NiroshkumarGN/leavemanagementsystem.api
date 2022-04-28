cce6999846d03bbd4a0dfc6e6ba531879fe914e4
NiroshkumarGN <niroshkumar1606@gmail.com>
2022-04-28 09:40:00 +0530

    leave management app

---
+++ src/main/java/com/leavemanagementsystem/LeavemanagementsystemApiApplication.java
@@ -0,0 +1,14 @@
+package com.leavemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeavemanagementsystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeavemanagementsystemApiApplication.class, args);
+		System.out.println("nirsoh");
+	}
+
+}
---
+++ src/test/java/com/leavemanagementsystem/LeavemanagementsystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leavemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeavemanagementsystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
